ad2f0d8108ce0bcb719c92a8cd815fdf882056c2
gabriel <metalmadnes98@gmail.com>
2022-04-24 17:35:43 -0500

    paso 2. creacion de archivo app para inicializar la aplicacion express

---
+++ app.js
@@ -0,0 +1,18 @@
+// se va a trabajar con objetos express
+const express = require('express')
+    // creacion de app express
+const app = express()
+app.use(express.json()) // usaremos json para trabajar
+    // asignacion de puerto a trabajar
+const port = 3000
+
+// creacion de routes
+app.get('/launchX', (req, res) => {
+    // respuesta que se tendra
+    res.send("Hola, Bienvenido a LaunchX");
+})
+
+// inicializacion de la app
+app.listen(port, () => {
+    console.log('app is running!!!!')
+})
