b051eeda2b8c3c9e334aa17d22e31040d80d382a
gabriel <metalmadnes98@gmail.com>
2022-04-24 17:49:28 -0500

    creacion de un endpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // se va a trabajar con objetos express
+const { response } = require('express')
 const express = require('express')
     // creacion de app express
 const app = express()
@@ -6,10 +7,22 @@ app.use(express.json()) // usaremos json para trabajar
     // asignacion de puerto a trabajar
 const port = 3000
 
-// creacion de routes
-app.get('/', (req, res) => {
-    // respuesta que se tendra
-    res.send("Hola, Bienvenido a LaunchX");
+// Creacion de un endpoint
+// metodos http => GET, POST, PUT, DELETE
+app.get('/v1/explorers', (req, res) => {
+    console.log(`GET Explorers V1 API ${new Date()}`)
+
+    // lista de explorers
+    const explorer = { id: 1, name: "ExplorerZ" } //objeto JS
+    const explorer1 = { id: 2, name: "ExplorerA" } //objeto JS
+    const explorer2 = { id: 3, name: "ExplorerB" } //objeto JS
+    const explorer3 = { id: 4, name: "ExplorerC" } //objeto JS
+    const explorers = [explorer, explorer1, explorer2, explorer3]
+
+    //se realiza un metodo html para poder realizar un request
+    // como se esta realizando un metodo get se debe de obtener un codigo html para indicar el estado de la peticion
+    // HTTP CODE STATUS: 200 => correcto
+    response.status(200).json(explorer)
 })
 
 // inicializacion de la app
